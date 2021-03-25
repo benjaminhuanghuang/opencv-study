a9e6eab65271c60290bb592aebb8ee8ceda8f101
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-03-25 13:16:20 -0700

    face detect

--- _notes/reference.md
+++ _notes/reference.md
@@ -1,4 +1,6 @@
 - [LEARN OPENCV C++ in 4 HOURS | Including 3x Example Projects Win/Mac (2021)](https://www.youtube.com/watch?v=2FYm3GOonhk&ab_channel=Murtaza%27sWorkshop-RoboticsandAI)
+  - https://github.com/murtazahassan/Learn-OpenCV-cpp-in-4-Hours
+  
 
 - [如何从0学习opencv,完成类似人脸检测的毕设？](https://www.zhihu.com/question/38536019)
 - [Python OpenCV: Face detection and counting](https://techtutorialsx.com/2017/05/02/python-opencv-face-detection-and-counting/)
@@ -9,4 +11,3 @@ https://blogs.wcode.org/2014/10/howto-install-build-and-use-opencv-macosx-10-10/
 
 - Setting Up OpenCV for C++ using CMake and VS Code on Mac OS
   - https://thecodinginterface.com/blog/opencv-cpp-vscode/
-  
--- cv-cpp-demo/img-contour.cpp
+++ cv-cpp-demo/img-contour.cpp
@@ -8,14 +8,51 @@
 using namespace cv;
 using namespace std;
 
-void getContours(Mat imgDil, Mat img)
-{
-  vector<vector<Point>> contours;
-  vector<Vec4i> hierarchy;
 
-  findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-  drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+void getContours(Mat imgDil, Mat img) {
+
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	 
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		cout << area << endl;
+		string objectType;
+
+    // Filter
+		if (area > 1000) 
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			cout << conPoly[i].size() << endl;
+			boundRect[i] = boundingRect(conPoly[i]);
+		
+			int objCor = (int)conPoly[i].size();
+
+			if (objCor == 3) { objectType = "Tri"; }
+			else if (objCor == 4)
+			{ 
+				float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+				cout << aspRatio << endl;
+				if (aspRatio> 0.95 && aspRatio< 1.05){ objectType = "Square"; }
+				else { objectType = "Rect";}
+			}
+			else if (objCor > 4) { objectType = "Circle"; }
+
+			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+			putText(img, objectType, { boundRect[i].x,boundRect[i].y - 5 }, FONT_HERSHEY_PLAIN,1, Scalar(0, 69, 255), 2);
+		}
+	}
 }
+
 
 int main()
 {
